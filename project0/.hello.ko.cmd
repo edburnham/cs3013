0bd5e3f8429a33282f4728d25aebd2e3b135d5b7
edburnham <ehburnham@wpi.edu>
2017-01-23 18:49:09 -0500

    First commit

---
+++ project0/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ehburnham/cs3013/project0/hello.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/ehburnham/cs3013/project0/hello.ko /home/ehburnham/cs3013/project0/hello.o /home/ehburnham/cs3013/project0/hello.mod.o
---
+++ project0/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/ehburnham/cs3013/project0/hello.ko
+/home/ehburnham/cs3013/project0/hello.o
