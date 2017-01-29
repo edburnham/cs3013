055c4d714f1fdf9e02b05e08acc90215dab469bf
mdcaldwell <mike86caldwell@gmail.com>
2017-01-29 11:05:08 -0500

    phase2 ready

--- project2/mckasperfee.c
+++ project2/mckasperfee.c
@@ -8,7 +8,7 @@
 unsigned long **sys_call_table;
 
 asmlinkage long (*ref_sys_cs3013_syscall1)(void);
-//asmlinkage long (*ref_sys_cs3013_syscall2)(void);
+asmlinkage long (*ref_sys_cs3013_syscall2)(void);
 asmlinkage long (*ref_sys_open)(const char* __user, int, int);
 asmlinkage long (*ref_sys_close)(int fd);
 asmlinkage long (*ref_sys_read)(int fd, void* buff, size_t cnt);
@@ -18,12 +18,10 @@ asmlinkage long new_sys_cs3013_syscall1(void) {
     return 0;
 }
 
-/*
 asmlinkage long new_sys_cs3013_syscall2(void) {
     printk(KERN_INFO "\"’Hello world?!’ More like ’Goodbye, CRUEL world!’ EXTERMINATE!\" -- Dalek");
     return 0;
 }
-*/
 
 asmlinkage long new_sys_open(const char* __user filename, int flags, int mode) {
     unsigned int uid = current_uid().val;
@@ -116,7 +114,7 @@ static int __init interceptor_start(void) {
     ref_sys_close = (void *)sys_call_table[__NR_close];
     ref_sys_read = (void *)sys_call_table[__NR_read];
     ref_sys_cs3013_syscall1 = (void *)sys_call_table[__NR_cs3013_syscall1];
-    //ref_sys_cs3013_syscall2 = (void *)sys_call_table[__NR_cs3013_syscall2];
+    ref_sys_cs3013_syscall2 = (void *)sys_call_table[__NR_cs3013_syscall2];
     
     /* Replace the existing system calls */
     disable_page_protection();
@@ -124,7 +122,7 @@ static int __init interceptor_start(void) {
     sys_call_table[__NR_close] = (unsigned long *)new_sys_close;
     sys_call_table[__NR_read] = (unsigned long *)new_sys_read;
     sys_call_table[__NR_cs3013_syscall1] = (unsigned long *)new_sys_cs3013_syscall1;
-    //sys_call_table[__NR_cs3013_syscall2] = (unsigned long *)new_sys_cs3013_syscall2;
+    sys_call_table[__NR_cs3013_syscall2] = (unsigned long *)new_sys_cs3013_syscall2;
     enable_page_protection();
     /* And indicate the load was successful */
     printk(KERN_INFO "Loaded interceptor!");
@@ -141,7 +139,7 @@ static void __exit interceptor_end(void) {
     sys_call_table[__NR_close] = (unsigned long *)ref_sys_close;
     sys_call_table[__NR_read] = (unsigned long *)ref_sys_read;
     sys_call_table[__NR_cs3013_syscall1] = (unsigned long *)ref_sys_cs3013_syscall1;
-    //sys_call_table[__NR_cs3013_syscall2] = (unsigned long *)ref_sys_cs3013_syscall2;
+    sys_call_table[__NR_cs3013_syscall2] = (unsigned long *)ref_sys_cs3013_syscall2;
     enable_page_protection();
     printk(KERN_INFO "Unloaded interceptor!");
 }
