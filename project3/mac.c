5417d2235108d950d7886e370a675b69f8c732c4
mdcaldwell <mike86caldwell@gmail.com>
2017-02-09 12:38:03 -0500

    moving along

--- project3/mac.c
+++ project3/mac.c
@@ -5,19 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <limits.h>
+#include <sys/types.h>
+#include <semaphore.h>
 
 #define SEED_VALUE 10
-#define NUM_NODES 100
+#define NUM_NODES 2
 #define NUM_NOISE_MAKERS 0
 
 /*random messages to send*/
 char* mess[15] = {"hello", "goodbye", "cs3013", "orange", "purple", "michael!", "edward!", "johnson!", "watson!", "danger!", "stranger!", "things!", "DOOM!", "highlander!", "whatThe!"};
 
-pthread_mutex_t channel_1lock;
-pthread_mutex_t channel_6lock;
-pthread_mutex_t channel_11lock;
+sem_t channel_1lock;
+sem_t channel_6lock;
+sem_t channel_11lock;
 
 typedef struct message{
     unsigned int nodeID;
@@ -31,6 +34,9 @@ typedef struct message{
 messageData channel_1[1000];
 messageData channel_6[1000];
 messageData channel_11[1000];
+int channel_1count = 0;
+int channel_6count = 0;
+int channel_11count = 0;
 
 messageData nodeCache[NUM_NODES];
 
@@ -40,11 +46,11 @@ typedef struct nodeData{
     int is_noisemaker;
     int x_coor;
     int y_coor;
-    double dwell_duration;
-    double dwell_probability;
-    double transmission_time;
-    double talk_window_time;
-    double talk_probability;
+    int dwell_duration;
+    int dwell_probability;
+    int transmission_time;
+    int talk_window_time;
+    int talk_probability;
     double dwell_noisemakers;
     double dwell_probability_noisemakers;
 }nodeData;
@@ -52,63 +58,163 @@ typedef struct nodeData{
 /*thread process*/
 void* nodeProcess(void* nodeInfo){
     nodeData* data = (nodeData*)nodeInfo;
-    //char* message;
+    int messageBuffCount = 0;
+    int channel = random() % 2;
+    useconds_t time = data->dwell_duration;
+    
     typedef struct inRange{
 	unsigned int nodeID;
 	int x_coor;
 	int y_coor;
     }inRange;
-
+    
+    messageData messageBuffer[100];
+    
     inRange nodesInRange[100];
     //char* messageBuff[100];
     int i;
     printf("---- NODE: %d, @X: %d, Y: %d ----\n", data->nodeID, data->x_coor, data->y_coor);
     //printf("0 ID: %d\n1 ID: %d\n", nodeCache[0].nodeID, nodeCache[1].nodeID);
-    for(i = 0; i < NUM_NODES; i++){// && (data->y_coor <= nodeCache[i].y_coor + 5) && (data->y_coor >= nodeCache[i].y_coor - 5)
-	if(data->nodeID != nodeCache[i].nodeID){	
-	    //printf("xcoor: %d ycoor: %d\n", nodeCache[i].x_coor, nodeCache[i].y_coor);
-	    if((data->x_coor <= nodeCache[i].x_coor + 5) && (data->x_coor >= nodeCache[i].x_coor - 5)&& (data->y_coor <= nodeCache[i].y_coor + 5) && (data->y_coor >= nodeCache[i].y_coor - 5)){
-		nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
-		nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
-		nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
-	    }
-	    else if((nodeCache[i].x_coor < 5 && data->x_coor < 5) && (nodeCache[i].y_coor > 95 && data->y_coor > 95) && (data->y_coor < (nodeCache[i].y_coor + 5))){
-		nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
-		nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
-		nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
-	    }
-	    else if((nodeCache[i].x_coor > 95 && data->x_coor > 95) && (nodeCache[i].y_coor < 5 && data->y_coor < 5) && (data->y_coor > (nodeCache[i].y_coor - 5))){
-		nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
-		nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
-		nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
-	    }
-	    else if((nodeCache[i].x_coor > 95 && data->x_coor > 95) && (nodeCache[i].y_coor > 95 && data->y_coor > 95) && (data->y_coor > (nodeCache[i].y_coor - 5))){
-		nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
-		nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
-		nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
-	    }
-	    else if((nodeCache[i].x_coor < 5 && data->x_coor < 5) && (nodeCache[i].y_coor < 5 && data->y_coor < 5) && (data->y_coor < (nodeCache[i].y_coor + 5))){
-		nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
-		nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
-		nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
-	    }
-	}
+    for(i = 0; i < NUM_NODES; i++){
+        if(data->nodeID != nodeCache[i].nodeID){	
+            //printf("xcoor: %d ycoor: %d\n", nodeCache[i].x_coor, nodeCache[i].y_coor);
+            if((data->x_coor <= nodeCache[i].x_coor + 5) && (data->x_coor >= nodeCache[i].x_coor - 5)&& (data->y_coor <= nodeCache[i].y_coor + 5) && (data->y_coor >= nodeCache[i].y_coor - 5)){
+            nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
+            nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
+            nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
+            }
+            else if((nodeCache[i].x_coor < 5 && data->x_coor < 5) && (nodeCache[i].y_coor > 95 && data->y_coor > 95) && (data->y_coor < (nodeCache[i].y_coor + 5))){
+            nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
+            nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
+            nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
+            }
+            else if((nodeCache[i].x_coor > 95 && data->x_coor > 95) && (nodeCache[i].y_coor < 5 && data->y_coor < 5) && (data->y_coor > (nodeCache[i].y_coor - 5))){
+            nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
+            nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
+            nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
+            }
+            else if((nodeCache[i].x_coor > 95 && data->x_coor > 95) && (nodeCache[i].y_coor > 95 && data->y_coor > 95) && (data->y_coor > (nodeCache[i].y_coor - 5))){
+            nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
+            nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
+            nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
+            }
+            else if((nodeCache[i].x_coor < 5 && data->x_coor < 5) && (nodeCache[i].y_coor < 5 && data->y_coor < 5) && (data->y_coor < (nodeCache[i].y_coor + 5))){
+            nodesInRange[i].nodeID = nodeCache[i].nodeID;//in range
+            nodesInRange[i].x_coor = nodeCache[i].x_coor;//in range
+            nodesInRange[i].y_coor = nodeCache[i].y_coor;//in range
+            }
+        }
     }
 	
     for(i = 0; i < NUM_NODES; i++){
-	if(nodesInRange[i].nodeID != 0) {
-	    printf("Node in range: %d, x: %d, y: %d\n", nodesInRange[i].nodeID,nodesInRange[i].x_coor,nodesInRange[i].y_coor);
-	}
+        if(nodesInRange[i].nodeID != 0) {
+            printf("Node in range: %d, x: %d, y: %d\n", nodesInRange[i].nodeID,nodesInRange[i].x_coor,nodesInRange[i].y_coor);
+        }
     }
-    //pthread_mutex_lock(&channel_1lock);
-    //pthread_mutex_unlock(&channel_1lock)
-	
-    /*
+
       while(1){
-		
-		
-      //message = strdup(mess[rand() % 14]); 
-      }*/
+          /*dwell duration, transmission time and random switching*/
+          if(((random() % 100) + 1) > data->dwell_probability){//decide whether to switch channels
+              channel = random() % 2;
+          }
+          if(channel == 0){//channel 1
+                  if(((random() % 100) + 1) < data->talk_probability){//decide whether to send message to global channel
+                        messageData newMessage;
+                        newMessage.transMess = strdup(mess[rand() % 14]);
+                        newMessage.nodeID = data->nodeID;
+                        newMessage.channel = 1;
+                        newMessage.x_coor = data->x_coor;
+                        newMessage.y_coor = data->y_coor;
+                        sem_wait(&channel_1lock);
+                        memcpy(&channel_1[channel_1count], &newMessage, sizeof(messageData));
+                        if(++channel_1count == 1000){
+                           channel_1count = 0;
+                        }
+                        sem_post(&channel_1lock);    
+                  }else{//local buffer retransmission
+                      sem_wait(&channel_1lock);
+                        memcpy(&channel_1[channel_1count], &messageBuffer[messageBuffCount], sizeof(messageData));
+                        if(++channel_1count == 1000){
+                           channel_1count = 0;
+                        }
+                      sem_post(&channel_1lock);
+                      if(++messageBuffCount == 100){
+                           messageBuffCount = 0;
+                      }
+                  }  
+          }else if(channel  == 1){//channel 6
+                if(((random() % 100) + 1) < data->talk_probability){//decide whether to send message
+
+                }
+               if(((random() % 100) + 1) < data->talk_probability){//decide whether to send message to global channel
+                        messageData newMessage;
+                        newMessage.transMess = strdup(mess[rand() % 14]);
+                        newMessage.nodeID = data->nodeID;
+                        newMessage.channel = 6;
+                        newMessage.x_coor = data->x_coor;
+                        newMessage.y_coor = data->y_coor;
+                        sem_wait(&channel_6lock);
+                        memcpy(&channel_6[channel_6count], &newMessage, sizeof(messageData));
+                        if(++channel_1count == 1000){
+                           channel_1count = 0;
+                        }
+                        sem_post(&channel_6lock);    
+                  }else{//local buffer retransmission
+                      sem_wait(&channel_6lock);
+                        memcpy(&channel_6[channel_6count], &messageBuffer[messageBuffCount], sizeof(messageData));
+                        if(++channel_6count == 1000){
+                           channel_6count = 0;
+                        }
+                      sem_post(&channel_6lock);
+                      if(++messageBuffCount == 100){
+                           messageBuffCount = 0;
+                      }
+                  }
+          }else if(channel == 2){//channel 11
+                if(((random() % 100) + 1) < data->talk_probability){//decide whether to send message
+                    
+                }
+               if(((random() % 100) + 1) < data->talk_probability){//decide whether to send message to global channel
+                        messageData newMessage;
+                        newMessage.transMess = strdup(mess[rand() % 14]);
+                        newMessage.nodeID = data->nodeID;
+                        newMessage.channel = 11;
+                        newMessage.x_coor = data->x_coor;
+                        newMessage.y_coor = data->y_coor;
+                        sem_wait(&channel_11lock);
+                        memcpy(&channel_11[channel_11count], &newMessage, sizeof(messageData));
+                        if(++channel_11count == 1000){
+                           channel_11count = 0;
+                        }
+                        sem_post(&channel_11lock);    
+                  }else{//local buffer retransmission
+                      sem_wait(&channel_11lock);
+                        memcpy(&channel_11[channel_11count], &messageBuffer[messageBuffCount], sizeof(messageData));
+                        if(++channel_11count == 1000){
+                           channel_11count = 0;
+                        }
+                      sem_post(&channel_11lock);
+                      if(++messageBuffCount == 100){
+                           messageBuffCount = 0;
+                      }
+                  }
+          }
+              
+        for(i = 0; i < channel_1count; i++){
+            if(channel_1[i].nodeID == ){
+                
+            }
+        }
+        for(i = 0; i < channel_6count; i++){
+            
+        }
+        for(i = 0; i < channel_11count; i++){
+            
+        }
+		usleep(time);//transmission time
+       
+          
+      }
 	
     pthread_exit(NULL);
 }
@@ -119,7 +225,11 @@ int main(int argc, char** argv){
 	
     pthread_t nodes[NUM_NODES];
     nodeData nodeInfo[NUM_NODES];//data struct for each thread
-	
+    
+	sem_init(&channel_1lock, 0, 1);
+    sem_init(&channel_6lock, 0, 1);
+    sem_init(&channel_11lock, 0, 1);
+    
     if(argc == 1){
 	for(i = 0; i < NUM_NODES; i++){
 	    nodeInfo[i].nodeID = random() % UINT_MAX;//set node Id, 4 bytes
@@ -138,11 +248,11 @@ int main(int argc, char** argv){
 	      nodeCache[i].x_coor = nodeInfo[i].x_coor;
 	      nodeCache[i].y_coor = nodeInfo[i].y_coor;*/
 			
-	    nodeInfo[i].dwell_duration = 1.0;
-	    nodeInfo[i].dwell_probability = 1.0;
-	    nodeInfo[i].transmission_time = 1.0;
-	    nodeInfo[i].talk_window_time = 1.0;
-	    nodeInfo[i].talk_probability = 1.0;
+	    nodeInfo[i].dwell_duration = 1000000;
+	    nodeInfo[i].dwell_probability = 50;
+	    nodeInfo[i].transmission_time = 1000000;
+	    nodeInfo[i].talk_window_time = 1000000;
+	    nodeInfo[i].talk_probability = 50;
 	    if(i < NUM_NOISE_MAKERS){//make desired amount of nodes noisemakers
 		nodeInfo[i].is_noisemaker = 1;
 		nodeInfo[i].dwell_noisemakers = 1.0;
