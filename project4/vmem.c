f711dfa5d0b24c904b2e738ee359970b7d5dd98d
edburnham <ehburnham@wpi.edu>
2017-02-17 16:40:52 -0500

    added vmem.c

---
+++ project4/vmem.c
@@ -0,0 +1,16 @@
+//Edward Burnham
+//Michael Caldwell
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SIZE 64
+
+char memory[SIZE];
+
+int main(int argc, char** argv){
+    
+    return 0;
+}
